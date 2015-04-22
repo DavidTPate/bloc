c2f19499a4ed4078ab5caba3d9fe74d6c18eddce
David Pate <davidtpate@gmail.com>
2015-04-21 21:28:01 -0400

    Started implementing what would be the comparison pieces.

--- test/query.js
+++ test/query.js
@@ -55,13 +55,43 @@ describe('query()', function () {
         }
     ];
 
+    it('should provide a friendly message on an invalid query', function (done) {
+
+        Bloc.query(testData, {
+            age: {
+                $eq: 42
+            }
+        }).then(function () {
+
+            done(new Error('Expected to throw'));
+        }, function (reason) {
+
+            expect(reason).to.be.instanceOf(Error);
+            expect(reason.message).to.equal('"age" is not allowed');
+            done();
+        });
+    });
+
+    it('should send back all results when there isn\'t a query', function(done) {
+
+       Bloc.query(testData).then(function(results) {
+
+           expect(results[0]).to.equal(testData[0]);
+           expect(results[1]).to.equal(testData[1]);
+           expect(results.length).to.equal(2);
+           done();
+       }, done);
+    });
+
     describe('$eq', function () {
 
         it('should be able to filter on a top level key', function (done) {
 
             Bloc.query(testData, {
-                age: {
-                    $eq: 42
+                $match: {
+                    age: {
+                        $eq: 42
+                    }
                 }
             }).then(function (results) {
 
@@ -74,11 +104,13 @@ describe('query()', function () {
         it('should be able to filter with multiple top level keys', function (done) {
 
             Bloc.query(testData, {
-                age: {
-                    $eq: 42
-                },
-                name: {
-                    $eq: 'Nigel Thornberry'
+                $match: {
+                    age: {
+                        $eq: 42
+                    },
+                    name: {
+                        $eq: 'Nigel Thornberry'
+                    }
                 }
             }).then(function (results) {
 
